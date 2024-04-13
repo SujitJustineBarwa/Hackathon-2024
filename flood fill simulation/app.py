bf8634007972855571a1e43c60d60c64507c5c0d
Justine <sujitjustine@gmail.com>
2024-04-13 10:26:45 +0530

    "v1.1.1  Changed the behaviour of delete wall function and added a custom alert to let us know if wall was deleted or not"

--- flood fill simulation/app.py
+++ flood fill simulation/app.py
@@ -2,20 +2,21 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-import dash_mantine_components as dmc
-import plotly.graph_objects as go
+import plotly.express as px
+import os
 from walls import walls
 from extended_functions import *
-import os
+import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 
-app = dash.Dash(__name__, external_stylesheets=['styles.css'])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 wall_pattern = walls()
 disk = memory()
 
 app.layout = html.Div(children=[
     html.Div([
         html.H1("Maze Builder", style={'textAlign': 'center', 'marginBottom': '20px'}),
-        html.P("Welcome to the Maze Builder app. Use the buttons and dropdowns below to interact with the maze.", 
+        html.P("Welcome to the Maze Builder app. Use the buttons and dropdowns below to interact with the maze.",
                style={'textAlign': 'center', 'marginBottom': '40px'}),
         html.Div(
             dcc.Graph(
@@ -27,29 +28,39 @@ app.layout = html.Div(children=[
         html.Div([
             html.Div(id='selected_node_monitor'),
             dmc.Button('Delete Wall', id='delete_wall_button', color='black', style={'marginRight': '10px'}),
+            dmc.Alert(id='alert-delete-wall', hide=True,duration=1000),
         ], style={'textAlign': 'center', 'marginBottom': '40px'}),
-        dmc.Button('Regenerate', id='regenerate_button', color='black', variant='filled', style={'marginBottom': '40px'}),
+        dmc.Button('Regenerate', id='regenerate_button', color='black', variant='filled',
+                   style={'marginBottom': '40px'}),
         html.Div([
             dcc.Input(id='save_filename', type='text', placeholder='Enter filename', style={'marginRight': '10px'}),
-            dmc.Button('Save Maze', id='save_button', color='blue', variant='filled', style={'marginRight': '10px'}),
+            dmc.Button('Save Maze', id='save_button', color='blue', variant='filled',
+                       style={'marginRight': '10px'}),
         ], style={'textAlign': 'center', 'marginBottom': '40px'}),
         html.Div([
-            dcc.Dropdown(id='load_dropdown', options=[], placeholder='Select file to load', style={'marginRight': '10px'}),
-            dmc.Button('Load Maze', id='load_button', color='blue', variant='filled', style={'marginRight': '10px'}),
+            dcc.Dropdown(id='load_dropdown', options=[], placeholder='Select file to load',
+                         style={'marginRight': '10px'}),
+            dmc.Button('Load Maze', id='load_button', color='blue', variant='filled',
+                       style={'marginRight': '10px'}),
         ], style={'textAlign': 'center', 'marginBottom': '40px'}),
         html.Div([
-            dcc.Dropdown(id='delete_dropdown', options=[], placeholder='Select file to delete', style={'marginRight': '10px'}),
-            dmc.Button('Delete Maze', id='delete_button', color='red', variant='filled', style={'marginRight': '10px'}),
+            dcc.Dropdown(id='delete_dropdown', options=[], placeholder='Select file to delete',
+                         style={'marginRight': '10px'}),
+            dmc.Button('Delete Maze', id='delete_button', color='red', variant='filled',
+                       style={'marginRight': '10px'}),
         ], style={'textAlign': 'center', 'marginBottom': '40px'}),
     ], style={'maxWidth': '800px', 'margin': 'auto'}),
 ], style={'padding': '20px'})
+
 
 @app.callback(
     [Output('grid-graph', 'figure'),
      Output('load_dropdown', 'options'),
      Output('delete_dropdown', 'options'),
      Output('selected_node_monitor', 'children'),
-     Output('delete_wall_button', 'color')],
+     Output('delete_wall_button', 'color'),
+     Output('alert-delete-wall', 'children'),
+     Output('alert-delete-wall', 'hide')],
     [Input('grid-graph', 'clickData'),
      Input('regenerate_button', 'n_clicks'),
      Input('save_button', 'n_clicks'),
@@ -84,7 +95,7 @@ def update_graph(clickData, regenerate_clicks, save_clicks, load_clicks, delete_
         # Save maze to a file
         if fig and save_filename:
             filename = save_filename + '.txt'
-            wall_pattern.to_text(filename)\
+            wall_pattern.to_text(filename)
 
     elif button_id == 'load_button':
         # Load maze from file
@@ -98,54 +109,69 @@ def update_graph(clickData, regenerate_clicks, save_clicks, load_clicks, delete_
             if os.path.exists(delete_filename):
                 os.remove(delete_filename)
 
-    disk.add("delete_wall_enable",False)
-    disk.add("selected_nodes",[])
-    disk.add("clicked_nodes",[])
-    disk.add("delete_wall_button_color",'')
+    # Setting the disk memory and delete wall button color
+    disk.add("delete_wall_enable", False)
+    disk.add("selected_nodes", [])
+    disk.add("clicked_nodes", [])
+    disk.add("delete_wall_button_color", '')
+    disk.add("alert_message", '')
+    disk.add("hide", False)
+    
+    disk.status["hide"] = True
+
+    if button_id == 'delete_wall_button':
+        if not disk.status["delete_wall_enable"]:
+            disk.status["delete_wall_enable"] = True
+            disk.status["delete_wall_button_color"] = 'red'
+        else:
+            disk.status["delete_wall_enable"] = False
+            disk.status["delete_wall_button_color"] = ''
+            
+            # Resetting the disk memory and delete wall button color
+            disk.status["selected_nodes"] = []
+            disk.status["clicked_nodes"] = []
+            
     if clickData and disk.status["delete_wall_enable"]:
         node_x, node_y = clickData['points'][0]['x'], clickData['points'][0]['y']
         node = wall_pattern.find_Node(node_x, node_y)
         disk.status["selected_nodes"].append(f"Selected Node ID: {node.node_Id}")
         disk.status["clicked_nodes"].append(node)
-        print(disk.status["clicked_nodes"])
-        
-        if len(disk.status["clicked_nodes"]) == 2:
-            wall_pattern.delete_wall(*disk.status["clicked_nodes"])
-            
+
+        if len(disk.status["clicked_nodes"]) >= 2:
+            return_status = wall_pattern.delete_wall(*disk.status["clicked_nodes"])
+            if return_status:
+                disk.status["alert_message"] = "Wall deleted !!!"
+            else:
+                disk.status["alert_message"] = "Wall not deleted !!!"
+            disk.status["hide"] = False
+
             # Resetting the disk memory and delete wall button color
             disk.status["selected_nodes"] = []
             disk.status["clicked_nodes"] = []
-            disk.status["delete_wall_enable"] = False
-            disk.status["delete_wall_button_color"] = ''
-
-    # Setting the disk memory and delete wall button color
-    if button_id == 'delete_wall_button':
-        if not disk.status["delete_wall_enable"]:
-            disk.status["delete_wall_enable"] = True
-            disk.status["delete_wall_button_color"] = 'red'
 
     # Replotting the walls
     if wall_pattern.wall_list:
-        for wall in wall_pattern.wall_list:                  
-                updated_fig.add_trace(wall.element())
-                
-    return updated_fig, load_options, delete_options, disk.status["selected_nodes"],disk.status["delete_wall_button_color"]
+        for wall in wall_pattern.wall_list:
+            updated_fig.add_trace(wall.element())
+
+    return updated_fig, load_options, delete_options, disk.status["selected_nodes"], disk.status["delete_wall_button_color"], disk.status["alert_message"], disk.status["hide"]
+
 
 def figure_template():
-    updated_fig = go.Figure()
-    
+    updated_fig = px.scatter()
     updated_fig.update_layout(
-                        height=750,
-                        width=850,
-                        margin=dict(l=0, r=0, t=0, b=0),
-                        paper_bgcolor="darkgrey",
-                        plot_bgcolor="black",
-                        showlegend=False
-                    )
+        height=750,
+        width=850,
+        margin=dict(l=0, r=0, t=0, b=0),
+        paper_bgcolor="darkgrey",
+        plot_bgcolor="black",
+        showlegend=False
+    )
 
     updated_fig.update_xaxes(visible=False, showgrid=False)
     updated_fig.update_yaxes(visible=False, showgrid=False)
-    return  updated_fig
+    return updated_fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
